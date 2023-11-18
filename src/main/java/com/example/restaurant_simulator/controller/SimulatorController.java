227fe6f7c02f440b17e2522052424535ae8476e9
shonsagoro <ramosproque@gmail.com>
2023-11-18 00:00:41 -0600

    some test

---
+++ src/main/java/com/example/restaurant_simulator/controller/SimulatorController.java
@@ -0,0 +1,11 @@
+package com.example.restaurant_simulator.controller;
+
+import javafx.stage.Stage;
+
+public class SimulatorController {
+    private Stage stage;
+
+    public SimulatorController(Stage stage){
+
+    }
+}
